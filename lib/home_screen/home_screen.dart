f0176fb039e28850317b6c81ada206d86fdba98d
WHJ125 <86822726+WHJ125@users.noreply.github.com>
2023-07-29 14:03:49 +0900

    Update home_screen.dart

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -37,131 +37,134 @@ class HomeScreen extends StatelessWidget {
               horizontal: 20,
               vertical: 20,
             ),
-            child: Column(
-              children: [
-                Container(
-                  height: 200,
-                  width: 200,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/kerby.gif'),
-                      fit: BoxFit.cover,
+            child: Expanded(
+              child: Column(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 200,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/kerby.gif'),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const IngredientChooser(),
-                        fullscreenDialog: true,
-                      ),
-                    );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 187, 196, 187),
-                            width: 2,
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const IngredientChooser(),
+                          fullscreenDialog: true,
+                        ),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: const Color.fromARGB(255, 187, 196, 187),
+                              width: 2,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 10,
+                                offset: const Offset(10, 10),
+                                color: Colors.black.withOpacity(0.2),
+                              ),
+                            ],
                           ),
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 10,
-                              offset: const Offset(10, 10),
-                              color: Colors.black.withOpacity(0.2),
-                            ),
-                          ],
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 70,
-                        ),
-                        child: const Column(
-                          children: [
-                            Image(
-                                width: 150,
-                                height: 150,
-                                image:
-                                    AssetImage('assets/images/vegetable.png')),
-                            Text(
-                              '루루카의 냉장고 속',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontFamily: 'Maplestory',
-                                  color: Color.fromARGB(255, 0, 0, 0),
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.w100),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const Category_select(),
-                        fullscreenDialog: true,
-                      ),
-                    );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 187, 196, 187),
-                            width: 2,
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 70,
                           ),
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 10,
-                              offset: const Offset(10, 10),
-                              color: Colors.black.withOpacity(0.2),
-                            ),
-                          ],
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 60,
-                        ),
-                        child: const Column(
-                          children: [
-                            Image(
-                                width: 150,
-                                height: 150,
-                                image: AssetImage('assets/images/cooking.png')),
-                            Text(
-                              '루루카의 요리 레시피',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontFamily: 'Maplestory',
-                                  color: Color.fromARGB(255, 0, 0, 0),
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.w100),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                          child: const Column(
+                            children: [
+                              Image(
+                                  width: 150,
+                                  height: 150,
+                                  image: AssetImage(
+                                      'assets/images/vegetable.png')),
+                              Text(
+                                '루루카의 냉장고 속',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontFamily: 'Maplestory',
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w100),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                )
-              ],
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Category_select(),
+                          fullscreenDialog: true,
+                        ),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: const Color.fromARGB(255, 187, 196, 187),
+                              width: 2,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 10,
+                                offset: const Offset(10, 10),
+                                color: Colors.black.withOpacity(0.2),
+                              ),
+                            ],
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 60,
+                          ),
+                          child: const Column(
+                            children: [
+                              Image(
+                                  width: 150,
+                                  height: 150,
+                                  image:
+                                      AssetImage('assets/images/cooking.png')),
+                              Text(
+                                '루루카의 요리 레시피',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontFamily: 'Maplestory',
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w100),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             )),
       ),
     );
